6b9ce63b45ed87e73583799c4a57c32cd1018270
sbeytour <beyour.safae@gmail.com>
2025-09-16 18:01:36 +0100

    auth controller

--- backend/src/main/java/blog/controllers/Authentication.java
+++ backend/src/main/java/blog/controllers/Authentication.java
@@ -1,24 +1,30 @@
 package blog.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import blog.dto.CreateUserDto;
+import blog.dto.RegisterUserDto;
 import blog.dto.UserResponseDto;
 import blog.services.UserService;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
-public class Register {
+public class Authentication {
     @Autowired
     UserService userService;
     
-    @PostMapping
-    public UserResponseDto createUser(@RequestBody @Valid CreateUserDto userDto) {
+    @PostMapping("/register")
+    public UserResponseDto createUser(@RequestBody @Valid RegisterUserDto userDto) {
         return userService.saveUser(userDto);
+    }
+
+    @GetMapping
+    public String Hello() {
+        return "Hello safae";
     }
 }
