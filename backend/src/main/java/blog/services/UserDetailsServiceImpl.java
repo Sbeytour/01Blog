a1abe10950e99b517456b7e8137b7f8117d1351c
sbeytour <beyour.safae@gmail.com>
2025-09-17 16:05:00 +0100

    add userDetails Servece impl

---
+++ backend/src/main/java/blog/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package blog.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import blog.repositories.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String identifier) throws UsernameNotFoundException {
+        try {
+            UserDetails user = userRepository.findByUsernameOrEmail(identifier);
+            return user;
+        } catch (UsernameNotFoundException e) {
+            throw new UsernameNotFoundException("User not found by identifier: " + identifier);
+        }
+    }
+}
--- backend/src/main/java/blog/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package blog.services;
-
-import blog.dto.RegisterUserDto;
-import blog.dto.UserResponseDto;
-
-public interface UserService {
-    public UserResponseDto saveUser(RegisterUserDto createUserDto);
-}
