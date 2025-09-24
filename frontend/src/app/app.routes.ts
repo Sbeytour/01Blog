0e32de71eae5e5284145e04bc55fdcacd519df42
sbeytour <beytour.safae@gmail.com>
2025-09-24 17:23:59 +0100

    add Frontend auth models

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "auth",
+        children: [
+            {
+                path: '',
+                redirectTo: 'login',
+                pathMatch: 'full'
+            },
+            {
+                path: 'login',
+                loadComponent: () => import('./features/auth/login/login').then(m => m.Login)
+            },
+            {
+                path: 'register',
+                loadComponent: () => import('./features/auth/register/register').then(m => m.Register)
+            }
+
+        ]
+    }
+];
--- frontend/src/app/core/models/auth.ts
+++ frontend/src/app/core/models/auth.ts
@@ -1,2 +1,21 @@
-export interface Auth {
+import { User } from "./user";
+
+export interface AuthResponce {
+    token: string;
+    userData: User;
+}
+
+export interface loginRequest {
+    identifier: string;
+    password: string;
+}
+
+export interface registerRequest {
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    bio: string;
+    profileImgUrl: string;
 }
