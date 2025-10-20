ddc66d54d4c6d800485ba7ca0492b807ea4c062f
sbeytour <beytour.safae@gmail.com>
2025-10-20 18:05:15 +0100

    update dialog data

--- frontend/src/app/components/dialog/dialog.ts
+++ frontend/src/app/components/dialog/dialog.ts
@@ -1,19 +1,25 @@
 import { Component, inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
-import { MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
 
+export interface dialogData {
+    title?: string,
+    message?: string
+}
 @Component({
     selector: 'app-dialog',
     templateUrl: './dialog.html',
     styleUrl: './dialog.scss',
     imports: [
         MatDialogModule,
-    MatButtonModule,
+        MatButtonModule,
 
     ]
 })
 export class DialogComponent {
-    private dialogRef = inject(MatDialogRef<DialogComponent>);
+   dialogRef = inject(MatDialogRef<DialogComponent>);
+   dialogData = inject<dialogData>(MAT_DIALOG_DATA);
+
     close(choice: boolean) {
         this.dialogRef.close(choice);
     }
